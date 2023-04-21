25d60b156bfe39fc5cf13bdf563d858b9a513759
Daniel Filat <filatdaniel98@gmail.com>
2023-04-21 10:55:38 +0300

    sets up zag ui and adds a basic button component with tailwind styles

---
+++ src/components/base/Pressable.tsx
@@ -0,0 +1,26 @@
+import * as pressable from "@zag-js/pressable"
+import {normalizeProps, useMachine} from "@zag-js/solid"
+import {createMemo, createUniqueId} from "solid-js"
+
+export const Pressable = () => {
+    const [state, send] = useMachine(
+        pressable.machine({
+            id: createUniqueId(),
+            onPress() {
+                console.log("press")
+            },
+            onLongPress() {
+                console.log("long press")
+            },
+        }),
+    )
+
+    const api = createMemo(() => pressable.connect(state, send, normalizeProps))
+
+    return (
+        <button {...api().pressableProps}
+                class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            {api().isPressed ? "Pressed!" : "Press Me"}
+        </button>
+    )
+}
