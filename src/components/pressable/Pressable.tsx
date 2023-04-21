114d5416c35c73101415385dfb4c96890717d7b6
Daniel Filat <filatdaniel98@gmail.com>
2023-04-21 15:49:08 +0300

    builds classes for pressable component

--- src/components/pressable/Pressable.tsx
+++ src/components/pressable/Pressable.tsx
@@ -1,27 +1,35 @@
-import * as pressable from "@zag-js/pressable"
-import {normalizeProps, useMachine} from "@zag-js/solid"
-import {createMemo, createUniqueId} from "solid-js"
+import * as pressable from "@zag-js/pressable";
+import { normalizeProps, useMachine } from "@zag-js/solid";
+import { createMemo, createUniqueId } from "solid-js";
+import { twMerge } from "tailwind-merge";
+import { pressableClass } from "~/components/pressable/pressable-classes";
 
 export const Pressable = () => {
-    const [state, send] = useMachine(
-        pressable.machine({
-            id: createUniqueId(),
-            onPress() {
-                console.log("press")
-            },
-            onLongPress() {
-                console.log("long press")
-            },
-            preventFocusOnPress: true
-        }),
-    )
+  const [state, send] = useMachine(
+    pressable.machine({
+      id: createUniqueId(),
+      onPress() {
+        console.log("press");
+      },
+      onLongPress() {
+        console.log("long press");
+      },
+      preventFocusOnPress: true,
+    })
+  );
 
-    const api = createMemo(() => pressable.connect(state, send, normalizeProps))
+  const api = createMemo(() => pressable.connect(state, send, normalizeProps));
 
-    return (
-        <button {...api().pressableProps}
-                class="bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-            {api().isPressed ? "Pressed!" : "Press Me"}
-        </button>
-    )
-}
+  return (
+    <button
+      {...api().pressableProps}
+      class={twMerge(
+        pressableClass.base,
+        pressableClass.size.xl,
+        pressableClass.variant.primary
+      )}
+    >
+      {api().isPressed ? "Pressed!" : "Press Me"}
+    </button>
+  );
+};
