3e2758a4c67b48d7e0db49c071f8a926ef82755c
Daniel Filat <filatdaniel98@gmail.com>
2023-04-22 16:53:40 +0300

    adds state for wether the page is scrolled

--- src/components/AppShell/NavBar.tsx
+++ src/components/AppShell/NavBar.tsx
@@ -1,14 +1,34 @@
 import { ChapterPicker } from '~/components/ChapterPicker'
+import { createSignal, onMount } from 'solid-js'
 
-export const NavBar = () => (
-    <nav class={'z-20 sticky top-0 bg-offWhite mx-auto w-full max-w-2xl px-6 lg:px-8'}>
-        <div class="border-b border-black">
-            <div class="flex flex-col sm:flex-row gap-6 pt-6 pb-4 sm:py-0 sm:h-16 justify-between items-center">
-                <div class="flex flex-shrink-0 items-center">
-                    <p class="text-md font-black">The Good Book</p>
-                </div>
-                <ChapterPicker value={'Geneza 1'} />
-            </div>
+const InteractiveNavbar = () => (
+    <div class="flex flex-col sm:flex-row gap-6 pt-6 pb-4 sm:py-0 sm:h-16 justify-between items-center">
+        <div class="flex flex-shrink-0 items-center">
+            <p class="text-md font-black">The Good Book</p>
         </div>
-    </nav>
+        <ChapterPicker value={'Geneza 1'} />
+    </div>
 )
+
+const [scrollPos, setScrollPos] = createSignal(false)
+
+export const NavBar = () => {
+    onMount(() => {
+        window.addEventListener('scroll', (e) => {
+            const scrollY = window.scrollY
+            console.log(scrollY)
+            if (!!scrollY !== scrollPos()) {
+                setScrollPos(!!scrollY)
+            }
+        })
+    })
+
+    return (
+        <nav class={'z-20 sticky top-0 bg-offWhite mx-auto w-full max-w-2xl px-6 lg:px-8'}>
+            <div class="border-b border-black">
+                <InteractiveNavbar />
+                {scrollPos() ? 'scrolled' : 'not scrolled'}
+            </div>
+        </nav>
+    )
+}
