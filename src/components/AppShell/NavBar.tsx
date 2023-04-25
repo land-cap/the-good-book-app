ce241486b69a60c16bdfbce133d21ce2860914bb
Daniel Filat <filatdaniel98@gmail.com>
2023-04-25 18:50:27 +0300

    shows scrolled navbar when interactive navbar is not in view

--- src/components/AppShell/NavBar.tsx
+++ src/components/AppShell/NavBar.tsx
@@ -1,15 +1,36 @@
-import { createSignal, onMount } from 'solid-js'
+import { createEffect, createSignal, onCleanup } from 'solid-js'
 import { twMerge } from 'tailwind-merge'
 import { Combobox } from '~/components'
 import { Portal } from 'solid-js/web'
 
-const [isScrolled, setIsScrolled] = createSignal(false)
+const [isInteractiveNavbarVisible, setIsInteractiveNavbarVisible] = createSignal(true)
 
-const transitionClass = 'transition-transform'
+const [interactiveNavbarEl, setInteractiveNavbar] = createSignal(null as unknown as HTMLElement)
+
+const intersectionObserver = new IntersectionObserver(
+    (entries) => {
+        const isIntersecting = entries[0].isIntersecting
+        setIsInteractiveNavbarVisible(isIntersecting)
+    },
+    { threshold: 0 }
+)
+
+createEffect(() => {
+    if (interactiveNavbarEl()) {
+        intersectionObserver.observe(interactiveNavbarEl())
+    }
+})
+
+onCleanup(() => {
+    intersectionObserver.unobserve(interactiveNavbarEl())
+})
 
 const InteractiveNavbar = () => {
     return (
-        <nav class={twMerge('bg-offWhite dark:bg-gray-800 mx-auto w-full max-w-3xl px-6 lg:px-8')}>
+        <nav
+            ref={(el) => setInteractiveNavbar(el)}
+            class={twMerge('bg-offWhite dark:bg-gray-800 mx-auto w-full max-w-3xl px-6 lg:px-8')}
+        >
             <div class={'border-b border-black dark:border-b-whiteOnDark'}>
                 <div class={'flex flex-col sm:flex-row gap-6 pt-6 pb-4 sm:py-0 sm:h-16 justify-between items-center'}>
                     <div class="flex flex-shrink-0 items-center">
@@ -34,14 +55,13 @@ const ScrolledNavbar = () => {
     return (
         <nav
             class={twMerge(
-                'z-10 fixed top-0 left-1/2 -translate-x-1/2 max-w-3xl bg-offWhite dark:bg-gray-800 mx-auto w-full px-6 lg:px-8',
-                transitionClass,
-                isScrolled()
+                'z-10 fixed top-0 left-1/2 -translate-x-1/2 max-w-3xl bg-offWhite dark:bg-gray-800 mx-auto w-full px-6 lg:px-8 transition-transform',
+                !isInteractiveNavbarVisible()
                     ? 'translate-y-0 ease-emphasized-decelerate duration-emphasized-decelerate'
                     : '-translate-y-full ease-emphasized-accelerate duration-emphasized-accelerate'
             )}
         >
-            <div class={twMerge('border-b border-gray-200 dark:border-b-gray-700', transitionClass)}>
+            <div class={'border-b border-gray-200 dark:border-b-gray-700'}>
                 <div class={twMerge('flex place-content-center py-2')}>
                     <p class="text-sm text-gray-500 font-black">Geneza 1</p>
                 </div>
@@ -51,15 +71,6 @@ const ScrolledNavbar = () => {
 }
 
 export const NavBar = () => {
-    onMount(() => {
-        window.addEventListener('scroll', (e) => {
-            const scrollY = window.scrollY
-            if (!!scrollY !== isScrolled()) {
-                setIsScrolled(!!scrollY)
-            }
-        })
-    })
-
     return (
         <>
             <Portal>
