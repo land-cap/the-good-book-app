67619786801343d82d2a340ad254d90572d15b66
Daniel Filat <filatdaniel98@gmail.com>
2023-04-26 10:01:36 +0300

    removes off white background

--- src/components/AppShell/AppShell.tsx
+++ src/components/AppShell/AppShell.tsx
@@ -4,11 +4,7 @@ import { Icon } from '~/components/composable/Icon'
 import { Capped } from '~/components/meta/Capped'
 
 export const AppShell = ({ children }: { children: JSX.Element }) => (
-    <div
-        class={
-            'text-base flex flex-col h-fit min-h-[100vh] text-black bg-offWhite dark:text-whiteOnDark dark:bg-gray-900'
-        }
-    >
+    <div class={'text-base flex flex-col h-fit min-h-[100vh] text-black dark:text-whiteOnDark dark:bg-gray-900'}>
         <NavBar />
         <div class="flex-grow mx-auto w-full max-w-3xl px-6 lg:px-8">{children}</div>
         <footer class="flex place-content-center my-24 sm:my-32">
