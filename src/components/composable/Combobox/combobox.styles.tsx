456a1d272cc913fbe96e5ba3b2b04674f6b2e590
Daniel Filat <filatdaniel98@gmail.com>
2023-04-27 08:13:47 +0300

    WIP

--- src/components/composable/Combobox/combobox.styles.tsx
+++ src/components/composable/Combobox/combobox.styles.tsx
@@ -4,7 +4,7 @@ export const defaultComboboxStyles = {
     inputButton: 'absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none text-gray-400',
     optionContainer:
         'absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm',
-    option: 'relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900',
+    option: 'relative cursor-pointer select-none py-2 pl-3 pr-9 text-gray-900',
     option_focused: 'bg-primary-600 text-white',
     option_checked: 'font-semibold',
     optionLabel: 'block truncate',
