496bd614d6ee0d903a072f5b30c685645055dc9c
Daniel Filat <filatdaniel98@gmail.com>
2023-05-05 19:35:38 +0300

    improves stuff

--- src/pages/Chapter/Chapter.page.tsx
+++ src/pages/Chapter/Chapter.page.tsx
@@ -1,12 +1,17 @@
 import {useIsBreakpoint} from '~/hooks/useIsBreakpoint'
 import {Capped} from '~/cap-ui/meta/Capped'
 import {useParams} from "@solidjs/router";
-import {createEffect, createSignal, For} from "solid-js";
+import {createEffect, createMemo, createSignal, For} from "solid-js";
 import {getChapter} from "~/bibleDataApi/bibleDataApi";
 
 const [book, setBook] = createSignal<string | null>(null)
 const [chapter, setChapter] = createSignal<number | null>(null)
 const [chapterData, setChapterData] = createSignal([])
+// @ts-ignore
+const chapterTitle = createMemo(() => chapterData()[0]?.content)
+
+createEffect(() => console.log(chapterTitle()))
+
 createEffect(() => {
     if (book() && chapter()) getChapter(book() as string, chapter() as number).then(setChapterData)
 })
@@ -23,12 +28,9 @@ const ChapterTitle = ({children}: { children: any }) => <Capped component="h1"
     {children}
 </Capped>
 
-const Verse = ({verse: {verseNumber, content}}: { verse: Verse }) => <><VerseNumber number={verseNumber}/>{content}</>
+const Verse = ({verse: {verseNumber, content}}: { verse: Verse }) => <>{' '}<VerseNumber
+    number={verseNumber}/>{' '}{content}</>
 
-const contentTypeToComponent = {
-    chapterTitle: ChapterTitle,
-    // verse: Verse
-}
 
 export const Chapter = () => {
     const isDesktop = useIsBreakpoint('sm')
@@ -44,58 +46,19 @@ export const Chapter = () => {
 
     return (
         <div class={'flex flex-col gap-12 mt-12'}>
-            <For each={chapterData()}>
-                {({type, content}) => {
-                    if (type === 'verse') return <Capped
-                        component={'p'}
-                        fontSize={isDesktop() ? 'xl' : 'base'}
-                        lineGap={isDesktop() ? 36 : 24}
-                        class="dark:text-gray-400"
-                    ><For each={content as Verse[]}>{verse => <Verse verse={verse}/>}</For></Capped>
-                    return contentTypeToComponent[type] ?
-                        // @ts-ignore
-                        <Dynamic
-                            // @ts-ignore
-                            component={contentTypeToComponent[type]}>{content}</Dynamic> : null
-                }}
-            </For>
-            {/*<Capped*/}
-            {/*    component={'p'}*/}
-            {/*    fontSize={isDesktop() ? 'xl' : 'base'}*/}
-            {/*    lineGap={isDesktop() ? 36 : 24}*/}
-            {/*    class="dark:text-gray-400"*/}
-            {/*>*/}
-            {/*    <VerseNumber number={1}/> La început, Dumnezeu a făcut cerurile și pământul.{' '}*/}
-            {/*    <VerseNumber number={2}/> Pământul era pustiu și gol; peste fața adâncului de ape era*/}
-            {/*    întuneric, și Duhul lui Dumnezeu Se mișca pe deasupra apelor. <VerseNumber number={3}/>{' '}*/}
-            {/*    Dumnezeu a zis: „Să fie lumină!” Și a fost lumină. <VerseNumber number={4}/> Dumnezeu a*/}
-            {/*    văzut că lumina era bună și Dumnezeu a despărțit lumina de întuneric.{' '}*/}
-            {/*    <VerseNumber number={5}/> Dumnezeu a numit lumina zi, iar întunericul l-a numit noapte.*/}
-            {/*    Astfel, a fost o seară și apoi a fost o dimineață: aceasta a fost ziua întâi.{' '}*/}
-            {/*    <VerseNumber number={6}/> Dumnezeu a zis: „Să fie o întindere între ape și ea să despartă*/}
-            {/*    apele de ape.” <VerseNumber number={7}/> Și Dumnezeu a făcut întinderea, și ea a despărțit*/}
-            {/*    apele care sunt dedesubtul întinderii de apele care sunt deasupra întinderii. Și așa a fost.*/}
-            {/*    <VerseNumber number={8}/> Dumnezeu a numit întinderea cer. Astfel, a fost o seară și apoi a*/}
-            {/*    fost o dimineață: aceasta a fost ziua a doua. <VerseNumber number={9}/> Dumnezeu a zis: „Să*/}
-            {/*    se strângă la un loc apele care sunt dedesubtul cerului și să se arate uscatul!” Și așa a*/}
-            {/*    fost. <VerseNumber number={10}/> Dumnezeu a numit uscatul pământ, iar grămada de ape a*/}
-            {/*    numit-o mări. Dumnezeu a văzut că lucrul acesta era bun. <VerseNumber number={11}/> Apoi*/}
-            {/*    Dumnezeu a zis: „Să dea pământul verdeață, iarbă cu sămânță, pomi roditori, care să facă rod*/}
-            {/*    după soiul lor și care să aibă în ei sămânța lor pe pământ.” Și așa a fost.{' '}*/}
-            {/*    <VerseNumber number={12}/> Pământul a dat verdeață, iarbă cu sămânță după soiul ei și pomi*/}
-            {/*    care fac rod și care își au sămânța în ei, după soiul lor. Dumnezeu a văzut că lucrul acesta*/}
-            {/*    era bun. <VerseNumber number={13}/> Astfel, a fost o seară și apoi a fost o dimineață:*/}
-            {/*    aceasta a fost ziua a treia. <VerseNumber number={14}/> Dumnezeu a zis: „Să fie niște*/}
-            {/*    luminători în întinderea cerului, ca să despartă ziua de noapte; ei să fie niște semne care*/}
-            {/*    să arate vremurile, zilele și anii <VerseNumber number={15}/> și să slujească de luminători*/}
-            {/*    în întinderea cerului, ca să lumineze pământul.” Și așa a fost. <VerseNumber number={16}/>{' '}*/}
-            {/*    Dumnezeu a făcut cei doi mari luminători, și anume: luminătorul cel mai mare, ca să*/}
-            {/*    stăpânească ziua, și luminătorul cel mai mic, ca să stăpânească noaptea; a făcut și stelele.{' '}*/}
-            {/*    <VerseNumber number={17}/> Dumnezeu i-a așezat în întinderea cerului, ca să lumineze*/}
-            {/*    pământul, <VerseNumber number={18}/> să stăpânească ziua și noaptea și să despartă lumina*/}
-            {/*    de întuneric. Dumnezeu a văzut că lucrul acesta era bun. <VerseNumber number={19}/> Astfel,*/}
-            {/*    a fost o seară și apoi a fost o dimineață: aceasta a fost ziua a patra.*/}
-            {/*</Capped>*/}
+            {chapterTitle() ? <ChapterTitle>{chapterTitle()}</ChapterTitle> : null}
+            <Capped
+                component={'p'}
+                fontSize={isDesktop() ? 'xl' : 'base'}
+                lineGap={isDesktop() ? 36 : 24}
+                class="dark:text-gray-400"
+            >
+                <For each={chapterData()}>
+                    {({type, content}) => {
+                        if (type === 'verse') return <For each={content as Verse[]}>{verse => <Verse
+                            verse={verse}/>}</For>
+                    }}
+                </For></Capped>
         </div>
     );
 }
