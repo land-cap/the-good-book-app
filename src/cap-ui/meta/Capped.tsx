640584f6678dcd24c2149af87cbac3ef7812b221
Daniel Filat <filatdaniel98@gmail.com>
2023-05-19 12:26:19 +0300

    wip

--- src/cap-ui/meta/Capped.tsx
+++ src/cap-ui/meta/Capped.tsx
@@ -2,7 +2,7 @@ import { Dynamic, DynamicProps } from 'solid-js/web'
 import { styled, StylesFn } from 'solid-styled-components'
 import { JSX, ValidComponent } from 'solid-js'
 import dmSansMetrics from '@capsizecss/metrics/dMSans'
-import dmSerifTextMetrics from '@capsizecss/metrics/dMSansSerifText'
+import dmSerifTextMetrics from '@capsizecss/metrics/dMSerifText'
 import { createStyleObject } from '@capsizecss/core'
 import { FontSize, fontSizeToCapHeight } from '~/config/fontSize'
 
