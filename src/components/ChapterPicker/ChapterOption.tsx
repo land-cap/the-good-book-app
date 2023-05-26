1a4d19b7780f1ac600f3663b4ded8cae3e81d62f
Daniel Filat <filatdaniel98@gmail.com>
2023-05-26 16:29:23 +0300

    adds proper style to chapter options

--- src/components/ChapterPicker/ChapterOption.tsx
+++ src/components/ChapterPicker/ChapterOption.tsx
@@ -5,12 +5,14 @@ export const ChapterOptions = (props: { chapterCount: number }) => {
 	const chapterList = createMemo(() => range(1, props.chapterCount).map((chapter) => chapter))
 
 	return (
-		<div class="flex flex-row flex-wrap">
+		<div class="grid grid-cols-5 gap-px bg-gray-200 border-y border-gray-200">
 			<For each={chapterList()}>{(chapter) => <ChapterOption value={chapter} />}</For>
 		</div>
 	)
 }
 
 export const ChapterOption = (props: { value: number }) => (
-	<button class="w-12 h-12 grid align-middle place-content-center">{props.value}</button>
+	<button class="grid aspect-square align-middle place-content-center bg-white font-bold">
+		{props.value}
+	</button>
 )
