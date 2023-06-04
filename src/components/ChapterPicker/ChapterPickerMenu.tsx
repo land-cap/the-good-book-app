e31e360cd29f5f4d6da3aa5a5759825594027ec8
Daniel Filat <filatdaniel98@gmail.com>
2023-06-04 11:37:49 +0300

    Update ChapterPickerMenu.tsx

--- src/components/ChapterPicker/ChapterPickerMenu.tsx
+++ src/components/ChapterPicker/ChapterPickerMenu.tsx
@@ -43,7 +43,7 @@ export const ChapterPickerMenu = (props: TChapterPickerMenuProps) => {
 	// 	}
 	// })
 
-	const maxHeightValue = createMemo(() => `calc(${windowHeight()}px - 116px + 12px - 96px )`)
+	const maxHeightValue = createMemo(() => `calc(${windowHeight()}px - 116px + 12px - 64px)`)
 
 	return (
 		<Presence exitBeforeEnter>
